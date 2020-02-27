054288150ef7f58ec186e078da5adaa6f4f7c650
chengjing <chengjing002@163.com>
2020-02-27 16:47:07 +0800

    登陆和退出功能

---
+++ src/components/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    Home 组件
+    <el-button type="info" @click="logout">exit</el-button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Home',
+    methods: {
+      logout () {
+        window.sessionStorage.clear()
+        this.$router.push('/login')
+        this.$message.success('退出成功')
+      }
+    }
+  }
+
+</script>
+
+<style lang="less" scoped>
+
+</style>
